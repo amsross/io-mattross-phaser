90f1bf48b5413a62f4671a3fc764fd25c3777111
Matt Ross <matt@mattross.io>
2014-09-13 16:56:33 +0000

    Update dev build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,38 @@ module.exports = function(grunt) {
 				'Gruntfile.js',
 			]
 		},
+		uglify: {
+			dist: {
+				files: {
+					'build/js/main.min.js': [
+						'assets/bower_components/phaser/build/phaser.js',
+						'assets/js/app.js',
+					]
+				},
+				options: {
+					sourceMap: true,
+					sourceMapIncludeSources: true,
+					sourceMapName: 'build/js/main.min.js.map'
+				}
+			}
+		},
+		less: {
+			dist: {
+				files: {
+					'build/css/main.min.css': [
+						'assets/less/game.less',
+					]
+				},
+				options: {
+					compress: true,
+					outputSourceFiles: true,
+					sourceMap: true,
+					sourceMapBasepath: '../../',
+					sourceMapFilename: 'build/css/main.min.css.map',
+					sourceMapURL: 'main.min.css.map'
+				}
+			}
+		},
 		imagemin: {
 			dist: {
 				options: {
@@ -71,10 +103,14 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', [
 		'jshint',
+		'uglify',
+		'less',
 		'imagemin',
 	]);
 
